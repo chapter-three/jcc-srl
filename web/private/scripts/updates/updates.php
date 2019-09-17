aaa9ad5c7dfb57bf7a0fdbe4c099f598a68ac5f2
Calvin Tyndall (sleepingmonk) <sleepingmonk@53471.no-reply.drupal.org>
2019-09-17 00:15:30 -0700

    Fix typo in quicksilver script.

--- web/private/scripts/updates/updates.php
+++ web/private/scripts/updates/updates.php
@@ -1,13 +1,14 @@
 <?php
-// Database Updates
-echo "Running database updates.\n"
+
+// Database Updates.
+echo "Running database updates.\n";
 passthru('drush updb -y');
-echo "Database updates have run.\n"
+echo "Database updates have run.\n";
 // Import all config changes.
 echo "Importing configuration from yml files...\n";
 passthru('drush config-import -y');
 echo "Import of configuration complete.\n";
-//Clear all cache
+// Clear all cache.
 echo "Rebuilding cache.\n";
 passthru('drush cr');
 echo "Rebuilding cache complete.\n";
