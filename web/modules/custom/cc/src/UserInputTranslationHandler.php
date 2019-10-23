3d429974e17fbba6f083f0466abf3be3fa171711
Brian Fisher <tbfisher@gmail.com>
2019-10-23 15:34:50 -0600

    initial commit

---
+++ web/modules/custom/cc/src/UserInputTranslationHandler.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Drupal\cc;
+
+use Drupal\content_translation\ContentTranslationHandler;
+
+/**
+ * Defines the translation handler for cc_user_input.
+ */
+class UserInputTranslationHandler extends ContentTranslationHandler {
+
+  // Override here the needed methods from ContentTranslationHandler.
+}
