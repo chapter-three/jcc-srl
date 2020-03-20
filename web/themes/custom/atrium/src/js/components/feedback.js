e83773882abb816d8e27dac37b2a204498552605
Zakiya <zakiya@chapterthree.com>
2020-03-20 16:58:15 -0700

    SRL-78 Completely dismiss box after form is submitted if close is clicked.

--- web/themes/custom/atrium/src/js/components/feedback.js
+++ web/themes/custom/atrium/src/js/components/feedback.js
@@ -1,29 +1,49 @@
 (function($) {
   "use strict";
-  
+
   Drupal.behaviors.feedback = {
     attach: function(context) {
-      const feedback_trigger = $('[data-feedback="trigger"]');
+      const feedback_trigger = $('[data-feedback^="trigger"]');
       const feedback_container = $('[data-feedback="container"]');
       const feedback_dialog = $('[data-feedback="dialog"]');
-      const feedback_confirmation = $('[data-feedback="container"] .webform-confirmation')
+      const feedback_confirmation = $(
+        '[data-feedback="container"] .webform-confirmation'
+      );
 
       const feedbackOpen = () => {
-        feedback_dialog.attr('open', 'open');
-        feedback_container.attr('open', 'open');
+        feedback_dialog.attr("open", "open");
+        feedback_container.attr("open", "open");
       };
-  
-      if (feedback_confirmation.length > 0 ) {
-        feedback_dialog.removeAttr('style');
-        feedbackOpen();
+
+      const feedbackDismiss = () => {
+        feedback_container.hide();
       };
-  
-      feedback_trigger.on('click', function(e) {
+
+      if (feedback_confirmation.length > 0) {
+        if (
+          sessionStorage.feedback_dismissed_page == window.location.pathname
+        ) {
+          feedbackDismiss();
+        } else {
+          feedback_dialog.removeAttr("style");
+          feedbackOpen();
+        }
+      }
+
+      feedback_trigger.on("click", function(e) {
         e.preventDefault();
-        if (feedback_dialog.attr('open')) {
-          feedback_container.css('transition', 'all .2s');
-          feedback_dialog.removeAttr('open');
-          feedback_container.removeAttr('open');
+        if (feedback_dialog.attr("open")) {
+          if (
+            feedback_confirmation.length > 0 &&
+            $(this).attr("data-feedback") == "trigger-close"
+          ) {
+            sessionStorage.feedback_dismissed_page = window.location.pathname;
+            feedbackDismiss();
+          } else {
+            feedback_container.css("transition", "all .2s");
+            feedback_dialog.removeAttr("open");
+            feedback_container.removeAttr("open");
+          }
         } else {
           feedbackOpen();
         }
