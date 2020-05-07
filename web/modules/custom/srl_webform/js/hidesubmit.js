fc1df80d054609c01952793adeeb69cb8fad32bb
Zakiya <zakiya@chapterthree.com>
2020-05-06 19:36:10 -0700

    SRL-114 Hide submit button.

--- web/modules/custom/srl_webform/js/hidesubmit.js
+++ web/modules/custom/srl_webform/js/hidesubmit.js
@@ -1,1 +1,21 @@
-console.log('submit');
+(function($, Drupal) {
+  "use strict";
+  
+  Drupal.behaviors.hidesubmit = {
+    attach: function(context, drupalSettings) {
+
+      const $formId = drupalSettings.srl_webform.id;
+      const $formClass = ".webform-submission-"+$formId+"-form";
+      const $form = $($formClass);
+      const $submitButton = $($formClass+" #edit-wizard-next, "+$formClass+" #edit-submit");
+      const $inputs = $($formClass+" .form-radios input")
+  
+      $inputs.on('change', function() {
+        $form.submit()
+      });
+      
+      $submitButton.hide();
+      
+    }
+  };
+})(jQuery, Drupal);
