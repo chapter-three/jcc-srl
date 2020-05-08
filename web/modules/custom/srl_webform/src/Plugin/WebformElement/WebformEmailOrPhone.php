b251302ab0bcdf928018da7522829e350719cee0
Zakiya <zakiya@chapterthree.com>
2020-05-08 12:13:22 -0700

    SRL-110 Scaffolding for custom email or phone component.

---
+++ web/modules/custom/srl_webform/src/Plugin/WebformElement/WebformEmailOrPhone.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace Drupal\srl_webform\Plugin\WebformElement;
+
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\webform\Plugin\WebformElementBase;
+use Drupal\webform\WebformSubmissionInterface;
+
+/**
+ * Provides a 'emailorphone' element.
+ *
+ * @WebformElement(
+ *   id = "emailorphone",
+ *   label = @Translation("Email or Phone"),
+ *   description = @Translation("Provides a field that can accept email or phone."),
+ *   category = @Translation("Advanced elements"),
+ * )
+ *
+ * @see \Drupal\webform\Plugin\WebformElementBase
+ * @see \Drupal\webform\Plugin\WebformElementInterface
+ * @see \Drupal\webform\Annotation\WebformElement
+ */
+class WebformEmailOrPhone extends WebformElementBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function defineDefaultProperties() {
+    // Here you define your webform element's default properties,
+    // which can be inherited.
+    //
+    // @see \Drupal\webform\Plugin\WebformElementBase::defaultProperties
+    // @see \Drupal\webform\Plugin\WebformElementBase::defaultBaseProperties
+    return [
+        'multiple' => '',
+        'size' => '',
+        'minlength' => '',
+        'maxlength' => '',
+        'placeholder' => '',
+      ] + parent::defineDefaultProperties();
+  }
+
+  /****************************************************************************/
+
+  /**
+   * {@inheritdoc}
+   */
+  public function prepare(array &$element, WebformSubmissionInterface $webform_submission = NULL) {
+    parent::prepare($element, $webform_submission);
+
+    // Here you can customize the webform element's properties.
+    // You can also customize the form/render element's properties via the
+    // FormElement.
+    //
+    // @see \Drupal\webform_example_element\Element\WebformExampleElement::processWebformElementExample
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function form(array $form, FormStateInterface $form_state) {
+    $form = parent::form($form, $form_state);
+    // Here you can define and alter a webform element's properties UI.
+    // Form element property visibility and default values are defined via
+    // ::defaultProperties.
+    //
+    // @see \Drupal\webform\Plugin\WebformElementBase::form
+    // @see \Drupal\webform\Plugin\WebformElement\TextBase::form
+    return $form;
+  }
+
+}
