fb62bdc301ca28cb2b25e968e14178525a21e582
Zakiya <zakiya@chapterthree.com>
2020-05-12 18:42:37 -0700

    SRL-110 Better formatting.

--- web/modules/custom/srl_webform/js/validatePhoneOrEmail.js
+++ web/modules/custom/srl_webform/js/validatePhoneOrEmail.js
@@ -4,35 +4,38 @@
   Drupal.behaviors.validatePhoneOrEmail = {
     attach: function(context) {
       
-      const $error = '<div class="form-item--error-message usa-error-message"><strong></strong></div>';
+      const $error = "<div class=\"form-item--error-message usa-error-message\"><strong>Please enter a valid email or phone number.</strong></div>";
+      const $inputEl = $(".js-form-type-emailorphone input");
   
-      const $formId = drupalSettings.srl_webform.id;
-      const $formClass = ".webform-submission-"+$formId.replace(/_/g,"-")+"-form";
-  
-      function validateEmail($input) {
-        if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test($input)) {
-          return (true)
+      const validateEmail = ($input) =>  {
+       if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test($input)) {
+          return true;
         }
-        return (false)
-      }
+        return false;
+      };
   
-      function validatePhone($input) {
+      const validatePhone = ($input) =>  {
         if (/^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/im.test($input)) {
-          return (true)
+          return true;
         }
-        return (false)
-      }
+        return false;
+      };
   
       function validatePhoneOrEmal($input) {
+
         validateEmail($input)
         validatePhone($input)
-        
+  
         console.log(validateEmail($input))
         console.log(validatePhone($input))
+        
+        if(validateEmail($input) == false || validatePhone($input) == false) {
+          // $(".js-form-type-emailorphone").insertAfter($error);
+        }
       }
 
-      $($formClass).on('submit', function() {
-        const $input = $(".js-form-type-emailorphone input").val()
+      $($inputEl).on('keyup', function(e) {
+        const $input = $(this).val()
         validatePhoneOrEmal($input)
       });
 
