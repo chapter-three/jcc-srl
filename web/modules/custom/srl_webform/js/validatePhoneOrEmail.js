fd580c069158210775cb1a3dca7a9ce2d78d220d
Zakiya <zakiya@chapterthree.com>
2020-05-12 18:00:16 -0700

    SRL-110 Functioning js tests.

--- web/modules/custom/srl_webform/js/validatePhoneOrEmail.js
+++ web/modules/custom/srl_webform/js/validatePhoneOrEmail.js
@@ -4,7 +4,38 @@
   Drupal.behaviors.validatePhoneOrEmail = {
     attach: function(context) {
       
-      console.log('validate')
+      const $error = '<div class="form-item--error-message usa-error-message"><strong></strong></div>';
+  
+      const $formId = drupalSettings.srl_webform.id;
+      const $formClass = ".webform-submission-"+$formId.replace(/_/g,"-")+"-form";
+  
+      function validateEmail($input) {
+        if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test($input)) {
+          return (true)
+        }
+        return (false)
+      }
+  
+      function validatePhone($input) {
+        if (/^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/im.test($input)) {
+          return (true)
+        }
+        return (false)
+      }
+  
+      function validatePhoneOrEmal($input) {
+        validateEmail($input)
+        validatePhone($input)
+        
+        console.log(validateEmail($input))
+        console.log(validatePhone($input))
+      }
+
+      $($formClass).on('submit', function() {
+        const $input = $(".js-form-type-emailorphone input").val()
+        validatePhoneOrEmal($input)
+      });
+
     }
   };
 })(jQuery, Drupal);
