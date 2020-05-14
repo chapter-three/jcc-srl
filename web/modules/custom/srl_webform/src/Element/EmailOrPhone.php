9c9ddb0b034fa69cba97ccc4262d9b3bc1248a96
Zakiya <zakiya@chapterthree.com>
2020-05-13 17:47:19 -0700

    SRL-110 Make Element name more accurate and remove unneeded code.

--- web/modules/custom/srl_webform/src/Element/EmailOrPhone.php
+++ web/modules/custom/srl_webform/src/Element/EmailOrPhone.php
@@ -7,7 +7,7 @@ use Drupal\Core\Render\Element\FormElement;
 use Drupal\Core\Form\FormStateInterface;
 
 /**
- * Provides a 'emailorphone'.
+ * Provides an 'emailorphone' element.
  *
  * @FormElement("emailorphone")
  *
@@ -16,7 +16,7 @@ use Drupal\Core\Form\FormStateInterface;
  * @see \Drupal\Core\Render\Element\RenderElement
  * @see https://api.drupal.org/api/drupal/namespace/Drupal%21Core%21Render%21Element
  */
-class WebformEmailOrPhone extends FormElement {
+class EmailOrPhone extends FormElement {
 
   /**
    * {@inheritdoc}
@@ -27,26 +27,14 @@ class WebformEmailOrPhone extends FormElement {
       '#input' => TRUE,
       '#size' => 60,
       '#process' => [
-        [$class, 'processWebformElementExample'],
         [$class, 'processAjaxForm'],
       ],
       '#element_validate' => [
         [$class, 'validateWebformExampleElement'],
       ],
-      '#pre_render' => [
-        [$class, 'preRenderWebformExampleElement'],
-      ],
       '#theme' => 'input__webform_example_element',
       '#theme_wrappers' => ['form_element'],
     ];
-  }
-
-  /**
-   * Processes a 'webform_example_element' element.
-   */
-  public static function processWebformElementExample(&$element, FormStateInterface $form_state, &$complete_form) {
-    // Here you can add and manipulate your element's properties and callbacks.
-    return $element;
   }
 
   /**
@@ -67,23 +55,4 @@ class WebformEmailOrPhone extends FormElement {
     }
 
   }
-
-  /**
-   * Prepares a #type 'email_multiple' render element for theme_element().
-   *
-   * @param array $element
-   *   An associative array containing the properties of the element.
-   *   Properties used: #title, #value, #description, #size, #maxlength,
-   *   #placeholder, #required, #attributes.
-   *
-   * @return array
-   *   The $element with prepared variables ready for theme_element().
-   */
-  public static function preRenderWebformExampleElement(array $element) {
-    $element['#attributes']['type'] = 'text';
-    Element::setAttributes($element, ['id', 'name', 'value', 'size', 'maxlength', 'placeholder']);
-    static::setAttributes($element, ['form-text', 'webform-example-element']);
-    return $element;
-  }
-
 }
