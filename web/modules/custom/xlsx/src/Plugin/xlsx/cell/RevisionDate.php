373f485ae5ea05b1b8da069634468f8527d429ef
Mel Wong <mel.wong@jud.ca.gov>
2022-03-08 15:04:15 -0800

    SRL-570: recommit importer and entity updates plus search api

---
+++ web/modules/custom/xlsx/src/Plugin/xlsx/cell/RevisionDate.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Drupal\xlsx\Plugin\xlsx\cell;
+
+use Drupal\xlsx\Plugin\XlsxCellBase;
+
+/**
+ * Default XLSX cell plugin.
+ *
+ * @XlsxCell(
+ *   id = "revision_date",
+ *   name = @Translation("Revision Date"),
+ *   description = @Translation("Process revision date fields"),
+ *   field_types = {
+ *     "link",
+ *   }
+ * )
+ */
+class RevisionDate extends AsIs {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function import($entity, $field_name, $value, $mapped_fields, $data_array, $worksheet_index) {
+    if ($entity->hasField($field_name)) {
+      $drupalDate = date('Y-m-d', time());
+      if (!empty($value)) {
+        $drupalDate = date('Y-m-d', strtotime($value));
+      }
+
+      return $drupalDate;
+    }
+  }
+
+}
