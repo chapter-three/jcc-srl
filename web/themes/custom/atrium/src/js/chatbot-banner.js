621fd7bf3ad25fd6c4e37585f533ff7dca22cd48
Mel Wong <mel.wong@jud.ca.gov>
2022-11-02 16:41:15 -0700

    SRL-673: adjust timing for mobile & alert dismiss option

--- web/themes/custom/atrium/src/js/chatbot-banner.js
+++ web/themes/custom/atrium/src/js/chatbot-banner.js
@@ -3,15 +3,19 @@
 
   Drupal.behaviors.chatbotBanner = {
     attach: function (context) {
-
+      const cookieId = 'jcc-banner--chatbot';
       $(document).ready(function() {
-        setTimeout(chatBotBanner, 500);
+        let delay = 300;
+        if (window.matchMedia('(min-width: 1024px)')) {
+          delay = 1000;
+        }
+        setTimeout(chatBotBanner, delay);
       });
 
       function chatBotBanner() {
         if ($('.jcc-chat').length != 0) {
-          $('.jcc-banner--chatbot').prependTo('.jcc-text-section__column-left');
-          $('.jcc-banner--chatbot').fadeIn(2000);
+          $('.' + cookieId).prependTo('.jcc-text-section__column-left');
+          $('.' + cookieId).fadeIn(3000);
         }
       }
     }
