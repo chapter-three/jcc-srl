384b9c989bd2ccb80214a16ffcdcb545e862830a
Reshma Dhane <reshmaingavale@gmail.com>
2023-08-09 13:02:43 -0500

    Changes in js script of date calculation

--- web/modules/custom/srl_webform/js/calculateDateFinal.js
+++ web/modules/custom/srl_webform/js/calculateDateFinal.js
@@ -39,14 +39,14 @@
         const utcSelectedDate = new Date(selected.getUTCFullYear(), selected.getUTCMonth(), selected.getUTCDate());
         const differenceInTime = todaysDate.getTime() - utcSelectedDate.getTime();
         const differenceInDays = differenceInTime / (1000 * 3600 * 24);
-        const formattedDate = utcSelectedDate.toLocaleString('en-us',{month:'short', day: 'numeric', year:'numeric'});
+        const formattedDate = utcSelectedDate.toLocaleString('en-us',{month:'long', day: 'numeric', year:'numeric'});
         const calculateDate = new Date(addDaysToDate(utcSelectedDate, 22));
-        const calculateDateFinal = calculateDate.toLocaleString('en-us',{month:'short', day: 'numeric', year:'numeric'});
+        const calculateDateFinal = calculateDate.toLocaleString('en-us',{month:'long', day: 'numeric', year:'numeric'});
         $(".selected_date").text(formattedDate);
         $(".calculate_date").text(calculateDateFinal);
 
         // Hide message box If selected date is more than 21 days back
-        if(differenceInDays <= 21) {
+        if(differenceInDays <= 22) {
           $formMessage.show();
         }else{
           $formMessage.hide();
